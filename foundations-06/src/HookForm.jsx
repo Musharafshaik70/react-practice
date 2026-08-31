253ae4097b6851a747387991960e5376bc9e73e4
Musharaf Shaik <musharafshaik630@gmail.com>
2026-08-31 07:39:18 +0530

    foundations-06 complete

--- foundations-06/src/HookForm.jsx
+++ foundations-06/src/HookForm.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 function HookForm() {
-  return (
-    <div>HookForm</div>
-  )
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitSuccessful, isSubmitting },
+    } = useForm({ defaultValues: { name: "Aman" }, mode: "onTouched" });
+
+    function submit(data) {
+        return newPromise((res, _) => console.log("Form Submitted", data));
+    }
+
+    if (isSubmitSuccessful) {
+        return (
+            <div>
+                <h2>Form Submitted Successfully</h2>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(submit)}>
+                <label>
+                    <input type="text" {...register("name", { required: "Name is Required" })} />
+                    {errors.name && <span>{errors.name.message}</span>}
+                </label>
+                <label>
+                    <input type="text" {...register("email", { required: "Email is Required" })} />
+                    {errors.email && <span>{errors.email.message}</span>}
+                </label>
+                <button type="submit" disabled={isSubmitting}>
+                    {isSubmitting ? "Submitting...." : "Submit"}
+                </button>
+            </form>
+        </div>
+    );
 }
 
-export default HookForm
+export default HookForm;
